d86fed2399c3ebd756aeb5edc4501aaa88da4438
Arelys <aalva456@fiu.edu>
2018-07-06 20:55:39 -0400

    added a menu

---
+++ multiplayerBlackjack/Assets/JoinMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JoinMenu : MonoBehaviour {
+
+	public void JoinGame()
+    {
+        //add the name of the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ multiplayerBlackjack/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void JoinMenu()
+    {
+        //call the join menu
+        Debug.Log("Spawn the join game menu");
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
