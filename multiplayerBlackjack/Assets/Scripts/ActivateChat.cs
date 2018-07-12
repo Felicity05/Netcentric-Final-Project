6082f2b049892705b3bac5090315280a49ff3ca1
Arelys Alvarez <aalva456@fiu.edu>
2018-07-12 11:22:36 -0400

    add chat dialog to game

---
+++ multiplayerBlackjack/Assets/Scripts/ActivateChat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateChat : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenChatDialog(){
+
+        animator.SetBool("Open", true); // opens dialog
+    }
+
+    public void CloseDialog(){
+
+        animator.SetBool("Open", false);
+
+        Debug.Log("closing chat");
+    }
+
+    public void AnimationComplete()
+    {
+        //deactivate chatDialog
+        //reactivate chatButton
+        //add enevent in animation
+    }
+}
